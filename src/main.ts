eef38e5b2d8ff9613d30c11e40ae5c796fd000a6
Ugasjuan <30699096+Ugasjuan@users.noreply.github.com>
2024-06-19 12:24:26 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = {
-        origin: "*",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        credentials: true
-      };
-  
-  app.useGlobalPipes(new ValidationPipe());
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    });
+    
+    app.useGlobalPipes(new ValidationPipe());
 
   //  app.useGlobalInterceptors(new LoggingInterceptor());
 
